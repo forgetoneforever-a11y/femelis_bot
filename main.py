c9abacd8c7932cb0c18d03dba5d8909c89453549
forgetoneforever-a11y <forget.one.forever@gmail.com>
2026-08-26 14:50:32 +0500

    Update main.py

--- main.py
+++ main.py
@@ -171,7 +171,7 @@ def process_webhook(update: dict):
                             "Content-Type": "application/json"
                         }
                         json_data = {
-                            "model": "llama-3.3-70b-versatile",
+                            "model": "llama-3.1-8b-instant",
                             "messages": [{"role": "user", "content": user_text}]
                         }
                         with httpx.Client() as httpx_client:
